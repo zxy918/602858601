d2c569d01f189300658e7f0be524f27aa1b7b51e
Xinyue Zhang <602858601@qq.com>
2022-09-09 22:21:43 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,7 +17,7 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
-    String language = args[0];
+    String language = args[3];
     String country = args[1];
     String name = args[2];
     Greeter greeter = new Greeter(language, country, name);
